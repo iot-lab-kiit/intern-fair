871e490c3f72e475e6887c6c0b23ca2951a339a9
KudoAkinichi <arookrishna@gmail.com>
2024-03-11 23:11:41 +0530

    Navbar and hero component

---
+++ client/src/components/Navbar/MobileMenuButton.js
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react';
+
+const MobileMenuButton = ({ onClick }) => {
+  return (
+    <div className="absolute inset-x-0 top-2 left-3 flex items-center sm:hidden">
+      <button
+        onClick={onClick}
+        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+        aria-expanded="false"
+      >
+        {/* Hamburger Icon */}
+        <svg
+          className="block h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default MobileMenuButton;
+
+
+
