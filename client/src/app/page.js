df8ee4b2a6583e4be5f4146c8d7eba98cd776910
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-03-13 00:27:37 +0530

    Maanas~Improved LearningResources Component and AutoSlider

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -12,18 +12,18 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-8 max-w-[100vw]">
 
-      <Navbar />
-      <Hero />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
       {/* <About /> */}
-      <Mission />
+      {/* <Mission /> */}
       <Courses />
       <LearningResources/>
       {/* <Faq /> */}
-      <About align1="flex-row" data={data} />
+      {/* <About align1="flex-row" data={data} />
       <Mission /> 
        <Courses />
       <BTS align1="flex-row-reverse" data={data}/>
-      <Faq />
+      <Faq /> */}
 
     </main>
   );
--- client/src/components/homepage/LearningResources/AutoSlider.jsx
+++ client/src/components/homepage/LearningResources/AutoSlider.jsx
@@ -1,38 +1,25 @@
 "use client";
 
 import React from "react";
-import {useEffect, useState} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
-import "swiper/css";
-import SwiperCore, {Autoplay} from "swiper";
-
+import SwiperCore from "swiper";
+import {Autoplay} from "swiper/modules";
 SwiperCore.use([Autoplay]);
 
-const Carousel = ({arr, speed}) => {
-    const [index, setIndex] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex + 1) % arr.length);
-        }, speed);
-
-        return () => clearInterval(interval);
-    }, [arr, speed]);
-
+const Carousel = ({arr, speed, direction}) => {
     return (
-        <Swiper spaceBetween={1} slidesPerView="auto" loop autoplay={{delay: speed}} className="swiper-container">
-            {arr.map((item, i) => (
-                <SwiperSlide key={i} className="swiper-slide">
-                    <a
-                        href="#"
-                        className={`swiper-item cursor-pointer box-border flex justify-center items-center gap-2 bg-white border-black hover:border-[#1F3DD9] rounded-full shadow-md px-10 py-3 mx-3 ${
-                            index === i ? "active" : ""
-                        }`}>
+        <>
+            <Swiper freeMode={true} spaceBetween={10} loop autoplay={{delay: 0, reverseDirection: direction}} className="swiper-wrapper" allowTouchMove={false} slidesPerView={5} speed={speed}>
+                {arr.map((item, i) => (
+                    <SwiperSlide
+                        key={i}
+                        style={{flexShrink: 1}}
+                        className="duration-300 px-10 my-2 text-center cursor-pointer border-[#1F3DD9]-2 border-2 hover:font-bold hover:border-[#1F3DD9] transition-all rounded-full swiper-text-clamp whitespace-nowrap mx-12 py-2 shrink">
                         {item}
-                    </a>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
     );
 };
 
