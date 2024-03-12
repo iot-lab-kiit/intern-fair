91db63fd74f905e70871d41b4e27fdebe6e343c3
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-03-12 11:24:49 +0530

    added keyframe for animations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,33 +1,36 @@
-// ** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ["Gilroy-Medium", "Gilroy-Bold", "Gilroy-Light"],
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        fontFamily: {
+            sans: ["Gilroy-Medium", "Gilroy-Bold", "Gilroy-Light"],
+        },
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            animation: {
+                "loop-scroll": "loop-scroll 15s linear infinite",
+            },
+            keyframes: {
+                "loop-scroll": {
+                    from: {transform: "translateX(0%)"},
+                    to: {transform: "translateX(-100%)"},
+                },
+            },
+            screens: {
+                mbMini: "290px",
+                mbXSmall: "400px",
+                mbMedSmall: "500px",
+                mbSmall: "600px",
+                mbMedium: "800px",
+                laptop: "1000px",
+                tbPortrait: "1200px",
+                tbLandscape: "1600px",
+                Desktop: "2000px",
+                lgDesktop: "2400px",
+            },
+        },
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      screens: {
-        mbMini: "290px",
-        mbXSmall: "400px",
-        mbMedSmall:"500px",
-        mbSmall: "600px",
-        mbMedium: "800px",
-        laptop:"1000px",
-        tbPortrait: "1200px",
-        tbLandscape: "1600px",
-        Desktop: "2000px",
-        lgDesktop: "2400px",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
