9ac0926c72720be4c58887e8597bf7510b78d183
Vidhi425 <hcpsvidhi25@gmail.com>
2024-04-30 19:41:40 +0530

    navigation-pill

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -7,15 +7,13 @@ import Image from "next/image";
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+  
 
   return (
     <>
       <div className="content flex justify-center items-center  max-w-full mx-auto p-[1.75rem] sm:p-[0rem] ">
-        <div className="flex flex-col justify-center items-center max-w-1/2 mx-auto gap-4 place-self-center">
-          <div className="back-button text-xl  flex  w-full sm:w-[70%] h-16 ">
+        <div className="flex flex-col justify-center  mx-auto gap-4 place-self-center max-h-screen">
+          <div className="back-button text-base  flex  w-full sm:w-[100%] h-16 ">
             <div className="back-button flex gap-3 items-center justify-center">
               <div className="h-5 w-5">
                 <Image src="/images/back.png" height={20} width={20} />
@@ -23,12 +21,12 @@ export default function Login() {
               <div>Back</div>
             </div>
           </div>
-          <div className="form-container  flex flex-col items-center justify-center gap-[1.8rem] h-auto w-full sm:w-full flex-grow-1 flex-shrink-0 ">
-            <div className="header flex flex-col gap-4 w-full sm:w-[70%] text-center sm:text-start">
-              <div className="font-extrabold text-3xl md:text-5xl">Login</div>
-              <div className="text-xl">Enter your email and password to sign in!</div>
+          <div className="form-container  flex flex-col items-center justify-center gap-[0.5rem] h-auto w-full sm:w-full flex-grow-1 flex-shrink-0 ">
+            <div className="header flex flex-col gap-2 w-full sm:w-[100%] text-start">
+              <div className="font-extrabold text-3xl ">Login</div>
+              <div className="text-base">Enter your email and password to sign in!</div>
             </div>
-            <div className="google-signup flex items-center justify-center bg-[#F4F5FA] w-[98%] sm:w-[70%] h-12 rounded-lg">
+            <div className="google-signup flex items-center justify-center bg-[#F4F5FA] w-[98%] sm:w-[100%] h-10 rounded-lg">
               <div className="mr-2">
                 <Image
                   src="/images/Group.png"
@@ -37,39 +35,48 @@ export default function Login() {
                   alt="google"
                 />
               </div>
-              <div className="text-xl">Sign in with Google</div>
+              <div className="text-base">Sign in with Google</div>
             </div>
             {/* Or with lines */}
-            <div className="flex flex-col w-[60%]  items-center">
+            {/* <div className="flex flex-col w-[100%] sm:w-[60%] h-[1.75rem] sm:h-[100%] items-center">
               <Image
                 src="/images/Separator.png"
-                height={500}
+                height={10000}
                 width={800}
                 alt="separator"
+                className="w-[100%] h-[100%]"
               />
-            </div>
-            <form className="flex  flex-col gap-3 w-full sm:w-[70%]">
-              <label htmlFor="email" className="text-[#182467] text-xl">
+            </div> */}
+              <div className="flex flex-row gap-2 w-[100%] sm:w-[100%] h-[1.75rem] sm:h-[100%] items-center">
+            <div className="separator w-[100%] h-[2px] bg-[#E0E5F2] my-4"></div>
+            <div className="text-gray-500 text-xs">or</div>
+            <div className="separator w-[100%] h-[2px] bg-[#E0E5F2] my-4"></div>
+          </div>
+           
+            <form className="flex  flex-col gap-2 w-full ">
+              <label htmlFor="email" className="text-[#182467] text-base">
                 Email*
               </label>
               <input
                 type="email"
                 id="email"
-                className="w-full h-12 border-2 rounded-lg px-4 focus:border-blue-500"
+                className="w-full h-10 border-2 rounded-lg px-4 focus:border-blue-500"
               />
-              <label htmlFor="password" className="text-[#182467] text-xl">
+              <label htmlFor="password" className="text-[#182467] text-base">
                 Password*
               </label>
               <div className="relative">
                 <input
                   id="password"
                   type={showPassword ? "text" : "password"}
-                  className="w-full h-12 border-2 rounded-lg px-4 focus:border-[#1F3DD9]"
+                  className="w-full h-10 border-2 rounded-lg px-4 focus:border-[#1F3DD9]"
                 />
                 <button
                   type="button"
                   className="absolute inset-y-0 right-0 pr-6 top-[50%] -translate-y-1/2"
-                  onClick={togglePasswordVisibility}
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
                 >
                   {showPassword ? (
                     <FaRegEye
@@ -102,10 +109,10 @@ export default function Login() {
                 </div>
               </div>
 
-              <button className="bg-[#1F3DD9]  text-white text-xl h-12 w-1/2 rounded-lg">
+              <button className="bg-[#1F3DD9]  text-white text-lg h-10 w-1/2 rounded-lg">
                 Sign In
               </button>
-              <div className=" w-full sm:w-[70%] text-base">
+              <div className=" w-full sm:w-[100%] text-base">
             Not registered yet?{" "}
             <a href="/signup" className="text-[#1F3DD9]">
               Create an Account
@@ -117,13 +124,14 @@ export default function Login() {
           
         </div>
 
-        <div className="image-container w-[45%] h-screen  hidden lg:block">
+        <div className="image-container w-[45%] max-h-screen  hidden lg:block">
           <Image
             src="/images/signup.png"
             alt="signup-image"
             height={500}
             width={500}
             style={{ width: "100%", height: "100%" }}
+            className="max-h-screen"
           />
         </div>
       </div>
---
+++ client/src/app/courses/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+import Navigation from "@/components/courses/Navigation/Navigation"
+const courses = () => {
+  return (
+    <>
+     <Navbar />
+  
+   
+       <Navigation />
+
+   
+    </>
+ 
+  );
+};
+
+export default courses;
