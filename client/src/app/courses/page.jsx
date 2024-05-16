b0692e1e8a084ed3b97ba1fe748be500c33b2884
Akangkha <akangkhasarkar@gmail.com>
2024-05-17 02:08:30 +0530

    Added util function

--- client/src/app/courses/page.jsx
+++ client/src/app/courses/page.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState, useEffect } from "react";
 import { getTopics, getSubtopicsByTopicId } from "@/actions/topic";
@@ -27,9 +26,6 @@ const Page = () => {
 
   return (
     <>
-
-      <Navbar />
-
       <Navigation />
       <div className="w-screen max-w-full flex flex-col items-center justify-center gap-6 mbMedium:px-16 mbSmall:px-5 mbMini:px-0 my-16">
         <ExploreComponent
