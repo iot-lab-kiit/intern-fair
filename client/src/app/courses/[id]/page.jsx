17e4f919dc75127b6b985fbe95720c5b2c4fb0c3
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-27 23:20:58 +0530

    Fixed useeffect issue

--- client/src/app/courses/[id]/page.jsx
+++ client/src/app/courses/[id]/page.jsx
@@ -1,76 +1,42 @@
 "use client";
-
 import ReactMarkdown from "react-markdown";
-import ReactHtmlParser, {
-  processNodes,
-  convertNodeToElement,
-  htmlparser2,
-} from "react-html-parser";
-import { useEffect, useState } from "react";
-import { getSubtopicsByTopicId } from "@/actions/topic";
-import content from "@/data/courses/content";
+import ReactHtmlParser, {processNodes, convertNodeToElement, htmlparser2} from "react-html-parser";
+import {useEffect, useState} from "react";
+import {getSubtopicsByTopicId} from "@/actions/topic";
+// import content from "@/data/courses/content";
 import CodeSnippet from "@/components/courses/CodeSnippet/CodeSnippet";
 
-const page = ({ params }) => {
-  // params = { id : uuid}
-  const [data, setData] = useState(content.data);
-  console.log(setData);
-  useEffect(() => {
-    const fetchSubTopics = async () => {
-      try {
-        const subTopicData = await getSubtopicsByTopicId(params.id);
-        setData(subTopicData.result);
-      } catch (error) {
-        console.error("Error fetching topics:", error);
-      }
-    };
-
-    fetchSubTopics();
-  }, []);
-
-  return (
-    <>
-      <div className="w-full mbMedium:w-[80%] p-10 px-6 mbXSmall:px-10 mbMedium:px-16 flex flex-col items-start justify-center gap-4 mbMedSmall:gap-6 mbMedium:gap-8 ">
-        <h1 className="text-base mbXSmall:text-lg mbMedSmall:text-xl mbMedium:text-2xl tbPortrait:text-3xl tbLandscape:text-4xl text-[#081245] font-semibold">
-          {content.title}
-        </h1>
-        {data.map((item) => (
-          <div
-            key={item.id}
-            className="w-full flex flex-col items-start justify-center gap-5"
-          >
-            <h1 className="text-[#081245] text-sm mbXSmall:text-sm mbMedSmall:text-base mbMedium:text-lg tbPortrait:text-xl tbLandscape:text-2xl font-semibold">
-              {item.name}
-            </h1>
-            <p className="text-[#09123E] font-medium text-xs mbMedSmall:text-sm mbMedium:text-base tbPortrait:text-lg tbLandscape:text-xl">
-              {item.description}
-            </p>
-            {/* <ReactMarkdown>{item.MD}</ReactMarkdown> */}
-            <br />
-            {/* <div>{ReactHtmlParser(item.description)}</div> */}
-            {item.snippet && (
-              <CodeSnippet language={item.language} code={item.MD} />
-            )}
-          </div>
-        ))}
-      </div>
-      {/* {
-        <div>
-          <h1>{data.name}</h1>
-          <>Markdown : </>
-          <ReactMarkdown>{data.MD}</ReactMarkdown>
-          <br />
-          <br />
-          <>HTML : </>
-          <div>{ReactHtmlParser(data.decription)}</div>
-          <br />
-          <br />
-          <>HTML 2: </>
-          <div dangerouslySetInnerHTML={{ __html: data.decription }}></div>
-        </div>
-      } */}
-    </>
-  );
+const page = ({params}) => {
+    const [data, setData] = useState();
+    useEffect(() => {
+        const fetchSubTopics = async () => {
+            try {
+                const subTopicData = await getSubtopicsByTopicId(params.id);
+                setData(subTopicData);
+            } catch (error) {
+                console.error("Error fetching topics:", error);
+            }
+        };
+        fetchSubTopics();
+    }, [params.id]);
+    console.log(data);
+    return (
+        <>
+            <div className="w-full mbMedium:w-[80%] p-10 px-6 mbXSmall:px-10 mbMedium:px-16 flex flex-col items-start justify-center gap-4 mbMedSmall:gap-6 mbMedium:gap-8 ">
+                <h1 className="text-base mbXSmall:text-lg mbMedSmall:text-xl mbMedium:text-2xl tbPortrait:text-3xl tbLandscape:text-4xl text-[#081245] font-semibold"></h1>
+                {data && (
+                    <div className="w-full flex flex-col items-start justify-center gap-5">
+                        <h1 className="text-[#081245] text-sm mbXSmall:text-sm mbMedSmall:text-base mbMedium:text-lg tbPortrait:text-xl tbLandscape:text-2xl font-semibold">{data.result.name}</h1>
+                        {/* <p className="text-[#09123E] font-medium text-xs mbMedSmall:text-sm mbMedium:text-base tbPortrait:text-lg tbLandscape:text-xl">{data.result.description}</p> */}
+                        {/* <ReactMarkdown>{item.MD}</ReactMarkdown> */}
+                        <br />
+                        <div>{ReactHtmlParser(data.result.decription)}</div>
+                        {data.result.MD && <CodeSnippet language="md" code={data.result.MD} />}
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default page;
