70434dc660897060bbcc54a41b5486fc044a3413
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-06-02 19:26:24 +0530

    Update on SubSubTopic

--- client/src/actions/topic.js
+++ client/src/actions/topic.js
@@ -5,16 +5,9 @@ import { readItem, readItems } from "@directus/sdk";
 // Get all topics
 export const getTopics = async () => {
   try {
-    // FIXME : Highly inefficient, need to optimize
     const result = await clientToken(process.env.TOKEN).request(
       readItems("Topic", {
-        fields: [
-          "id",
-          "name",
-          {
-            subTopicID: ["id", "name"],
-          },
-        ],
+        fields: ["id", "name", { subTopicID: ["id", "name"] }],
       })
     );
     if (!result) throw new Error("No topics found");
@@ -48,14 +41,48 @@ export const getSubtopicsByTopicId = async (subtopicIds) => {
         fields: ["name", "MD", "decription"],
       })
     );
-
+    if (!subTopicData) throw new Error("No Subtopics found");
     return {
       success: true,
       message: "Found subtopics by IDs",
       result: subTopicData,
     };
-  } catch (e) {
-    console.log(e);
-    throw new Error(e.errors[0].message || e.message);
+  } catch (error) {
+    console.log(error);
+    // throw new Error(e.errors[0].message || e.message);
+  }
+};
+
+// Get subtopics by their IDs
+export const getsubSubtopics = async (subtopicIds) => {
+  try {
+    const subSubTopicData = await clientToken(process.env.TOKEN).request(
+      readItem("SubTopic", subtopicIds, {
+        fields: [
+          "id",
+          "name",
+          {
+            subSubTopicID: [
+              "id",
+              "subTitle",
+              "content",
+              "code_snippet",
+              "code_language",
+              "images",
+              "videos",
+            ],
+          },
+        ],
+      })
+    );
+    if (!subSubTopicData) throw new Error("No Subtopics found");
+    return {
+      success: true,
+      message: "Found subtopics by IDs",
+      result: subSubTopicData,
+    };
+  } catch (error) {
+    console.log(error);
+    // throw new Error(e.errors[0].message || e.message);
   }
 };
