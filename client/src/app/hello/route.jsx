9e460fb2c243a244cefe8ae5b93364f4043e24e5
Jatin Chaudhary <09jatin2005@gmail.com>
2024-06-04 18:47:17 +0530

    styling and integration done for /courses/[id]

--- client/src/app/hello/route.jsx
+++ client/src/app/hello/route.jsx
@@ -1,6 +1,6 @@
 import { getsubSubtopics, getDummySubSubtopics } from "@/actions/topic";
 export async function GET() {
-  const subTopicData = await getDummySubSubtopics(
+  const subTopicData = await getsubSubtopics(
     "5c79df97-8074-42f6-97cf-6113e06934ad"
   );
   return Response.json(subTopicData);
