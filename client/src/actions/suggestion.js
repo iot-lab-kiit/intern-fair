aec063581d90c1b84ebceea52994e209ff6fda5a
Kumar Priyanshu <109672617+kpriyanshu2003@users.noreply.github.com>
2024-07-30 19:06:27 +0000

    Fixed Backend Functions

--- client/src/actions/suggestion.js
+++ client/src/actions/suggestion.js
@@ -1,40 +1,39 @@
-"use server"
+"use server";
 
-import { clientToken } from "@/db/directus";
 import { createItem } from "@directus/sdk";
+import { clientToken } from "@/db/directus";
 
 const validateData = (data) => {
-    if (!data.name || !data.email || !data.suggestion)
-        throw new Error([{ message: "Missing Required Fields: Name, Email and Suggestion" }]);
+  if (!data.name || !data.email || !data.suggestion)
+    throw new Error("Missing Required Fields: Name, Email and Suggestion");
 
-    if (typeof data.name !== "string" || typeof data.email !== "string")
-        throw new Error([{ message: "Name and Email must be a string" }]);
+  if (typeof data.name !== "string" || typeof data.email !== "string")
+    throw new Error("Name and Email must be a string");
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(data.email)) {
-        throw new Error([{ message: "Invalid Email Format" }]);
-    }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(data.email)) throw new Error("Invalid Email Format");
 };
 
-export const createSuggestion=async (data)=>{
-    try {
-        validateData(data);
-        const suggestionResponse = await clientToken(process.env.TOKEN).request(
-            createItem("Suggestion", {
-                name: data.name,
-                email: data.email,
-                suggestion: data.suggestion,
-            })
-        );
-        if (!suggestionResponse) throw new Error("Suggestion not created");
+export const createSuggestion = async (data) => {
+  try {
+    validateData(data);
+    const suggestionResponse = await clientToken(process.env.TOKEN).request(
+      createItem("Suggestion", {
+        name: data.name,
+        email: data.email,
+        suggestion: data.suggestion,
+      })
+    );
 
-        return {
-            success: true,
-            message: "Suggestion created successfully",
-            result: suggestionResponse,
-        };
-    } catch (e) {
-        console.error("Error in createSuggestion:", e);
-        throw new Error(e.errors?.[0]?.message || e.message);
-    }
-}
+    if (!suggestionResponse) throw new Error("Suggestion not created");
+
+    return {
+      success: true,
+      message: "Suggestion created successfully",
+      result: suggestionResponse,
+    };
+  } catch (e) {
+    console.error(e);
+    throw new Error(e.errors?.[0]?.message || e.message);
+  }
+};
